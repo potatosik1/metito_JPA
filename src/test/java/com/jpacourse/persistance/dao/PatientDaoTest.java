b21ed2dc750454ba59489748cf5c75458029e467
Koksowiec <szczepanskikrzysiu@hotmail.com>
2025-04-27 15:01:33 +0200

    Zad 3 - testy

--- src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
+++ src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
@@ -39,5 +40,44 @@ public class PatientDaoTest {
         assertThat(patientEntity).isNotNull();
         VisitEntity visitEntity = patientEntity.getVisits().stream().filter(v -> v.getDescription().equals("TEST")).findAny().orElse(null);
         assertThat(visitEntity).isNotNull();
+    }
+
+    @Transactional
+    @Test
+    public void getPatientBySurname_WhenExecuted_FindPatientBySurname(){
+        // Arrange
+        String surname = "Nowak";
+
+        // Act
+        PatientEntity patientEntity = patientDao.getPatientBySurname(surname);
+
+        //Assert
+        assertThat(patientEntity).isNotNull();
+    }
+
+    @Transactional
+    @Test
+    public void getPatientsWithMoreVisitsThanX_WhenExecuted_FindPatientsWithMoreVisitsThanX(){
+        // Arrange
+        Long visitsAmount = 1L;
+
+        // Act
+        Collection<PatientEntity> patients = patientDao.getPatientsWithMoreVisitsThanX(visitsAmount);
+
+        // Assert
+        assertThat(patients.size()).isGreaterThan(0);
+    }
+
+    @Transactional
+    @Test
+    public void getPatientsWithVisitsAfterX_WhenExecuted_FindPatientsWithMoreVisitsThanX(){
+        // Arrange
+        LocalDate date = LocalDate.of(1999, 01, 01);
+
+        // Act
+        Collection<PatientEntity> patients = patientDao.getPatientsWithVisitsAfterX(date);
+
+        // Assert
+        assertThat(patients.size()).isGreaterThan(0);
     }
 }
